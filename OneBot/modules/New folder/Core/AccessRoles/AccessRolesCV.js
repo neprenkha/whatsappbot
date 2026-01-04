c3cb6bf47bfc1d1f634c156c1630a8d74f25fcbb
neprenkha <neprenkha@gmail.com>
2026-01-04 12:50:12 +0800

    Delete OneBot/modules/New folder directory

--- OneBot/modules/New folder/Core/AccessRoles/AccessRolesCV.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const V1 = require('./AccessRolesV1');
-
-async function init(meta) {
-  return V1.init(meta);
-}
-
-module.exports = { init };
--- OneBot/modules/New folder/Core/OutboundGateway/OutboundGatewayCV.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-module.exports = require("./OutboundGatewayV1");
