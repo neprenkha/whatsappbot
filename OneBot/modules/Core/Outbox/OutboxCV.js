a858012b1d037deab24c56d31a4e70f485c42fe8
neprenkha <neprenkha@gmail.com>
2026-03-02 04:54:42 +0800

    Delete OneBot/modules/Core directory

--- OneBot/modules/Core/Outbox/OutboxCV.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// CV wrapper for Outbox.
-// Hub requires module.exports.init(meta).
-// OutboxV1 exports init(meta) as a function.
-
-const init = require("./OutboxV1");
-
-module.exports = { init };
--- OneBot/modules/Core/TimeZone/TimeZoneCV.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const V1 = require('./TimeZoneV1');
-
-async function init(meta) {
-  return V1.init(meta);
-}
-
-module.exports = { init };
